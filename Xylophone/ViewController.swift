878207caaf182819039da9796f4e827c49a7b797
Chad McKinney <cfmckinney@gmail.com>
2020-02-06 12:45:23 -0700

    Connected buttons with action and sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound((sender.currentTitle)!)
+    }
     
-
+    func playSound(_ buttonPressed: String) {
+        let url = Bundle.main.url(forResource: buttonPressed, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
